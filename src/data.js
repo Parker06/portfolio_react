ac4e759dc306f0a87d1f31c6498d80fa3174c250
harry-parker6 <72020025+harry-parker6@users.noreply.github.com>
2021-10-25 14:22:32 +0100

    Added Initial commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+    {
+        title: "CySecApp",
+        subtitle: "HTML, CSS, JavaScript and Firebase",
+        description: "A security Dashboard that I made for my Web Development Module. It's a single page web application, which displays data about how many users connect to the systems, system statuses and health, security alerts and system alerts.",
+        image: "/images/project-1.gif",
+        link: "https://cysecapp.azurewebsites.net/",
+    },
+    {
+        title: "Commandline Dungeon Crawler",
+        subtitle: "C#",
+        description: "A C# Command line Dungeon Crawler that I made for my Software Engineering Module. It's a game where the player is in a maze surrounded by monsters and they need to get to the exit, they can pick up gold and kill monsters to get extra points.",
+        image: "/images/project-2.gif",
+    },
+    {
+        
+        title: "Eyes of Caelum",
+        subtitle: "HTML, CSS, JavaScript and Firebase",
+        description: "A responsive website that displays the information and work of the Plymouth based music band, Eyes of Caelum",
+        image: "/images/project-3.gif",
+        link: "https://eyesofcaelum.com",
+    },
+];
+
+export const skills = [
+    {
+        technology: "React",
+    },
+    {
+        technology: "HTML, CSS",
+    },
+    {
+        technology: "JavaScript",
+    },
+    {
+        technology: "Google Firebase",
+    },
+    {
+        technology: "Bootstrap",
+    },
+    {
+        technology: "C#",
+    }
+]
