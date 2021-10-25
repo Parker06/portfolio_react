01daf8e26ba46464eb23a333c09e7bc91ac2b63b
harry-parker6 <72020025+harry-parker6@users.noreply.github.com>
2021-10-25 17:37:13 +0100

    Removed unused components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import React from 'react';
 
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Development from "./components/Development";
-import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-import Scrollspy from "./components/Scrollspy";
 import Skills from './components/Skills';
-import Testimonials from "./components/Testimonials";
 
 
 export default function App(){
