3dc2956aa2b3179049fd1b2e11b23a6727e2dcae
harry-parker6 <72020025+harry-parker6@users.noreply.github.com>
2021-11-04 17:19:47 +0000

    Removed Development

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
 
 //files for website
 
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Development from "./components/Development";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from './components/Skills';
--- src/components/Development.js
+++ src/components/Development.js
@@ -1,1 +1,16 @@
-export default function Development() {}
+import React from 'react';
+
+export default function Development() {
+    return(
+        <section id="development" className="relative blue-500">
+            <div className="container px-5 py-10 mx-auto lg:px-40">
+                <div className="flex flex-col w-full mb-20">
+                    <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white text-center">Development</h1>
+                    <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-center">
+                        This will show the development process for my portfolio and how I go about executing the development process of a website.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
