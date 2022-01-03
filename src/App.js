dc5576f8b45cea076ab6333f3cebdb376e7d73be
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-01-03 18:07:03 +0000

    removed scroll parallax

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@
 import React, { useState, useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Development from "./components/Development";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from './components/Skills';
@@ -14,8 +13,7 @@ import "./index.css"
 
 
 export default function App(){
-
-  return (
+    return (
       <main className="text-gray-400" style={{backgroundColor: '#3E63F5'}}>
         <Navbar/>
         <About/>
